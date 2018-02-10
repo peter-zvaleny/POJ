25dc14b9f3122dce1c14416302192952a8782c38
Yijun Liu <yl2658@cornell.edu>
2018-02-10 18:01:43 +0800

    DFS recursive sol for lake counting

---
+++ lake_counting_2386.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+const int row_max = 100;
+const int col_max = 100;
+char water[row_max][col_max+1];
+
+
+void dfs(int y, int x){
+    if(y < 0 || y > n || x < 0 || x > m || water[y][x]!='W')
+        return;
+
+    water[y][x] = '.';
+
+    for(int dy = -1; dy <= 1; dy++){
+        for(int dx = -1; dx <= 1; dx++){
+             int pos_y = y + dy;
+             int pos_x = x + dx;
+             dfs(pos_y, pos_x);
+        }
+    }
+}
+
+int main(){
+    int cnt = 0;
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++){
+        scanf("%s", water[i]);
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(water[i][j] == 'W'){
+                dfs(i, j);
+                cnt++;
+            }
+        }
+    }
+
+    cout << cnt << endl;
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 ## POJ
 Start exercising with POJ.
+
+### Record
+| # | Name |  Tag1 | Tag2 | Date | Summary |
+| ----- | ---------- | ------- | ----- | ----| ------- |
+2386| Lake Counting| Graph | Search | 2.10 | 0. Similar to Number of Islands 200 in Leetcode 1. Assign to opposite symbol to avoid the use of `visited` matrix 2. How to iterate 8 neighbors more conveniently 3. read 2D array line by line in cpp|
+
